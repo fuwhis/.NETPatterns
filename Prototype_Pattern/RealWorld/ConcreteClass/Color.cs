34e451f6b02e2db0c3325d9cb0e08746e650c2e1
Quy Nguyen <steve.parker117@gmail.com>
2022-04-25 16:38:58 +0700

    `build`: add structure-code and real-world code of Prototype design pattern

---
+++ Prototype_Pattern/RealWorld/ConcreteClass/Color.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Pattern.RealWorld.ConcreteClass
+{
+    /// <summary>
+    /// The 'ConcretePrototype' class
+    /// </summary>
+    public class Color : ColorPrototype
+    {
+        int red;
+        int green;
+        int blue;
+
+        // Constructor
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        // Create a shallow copy
+        public override ColorPrototype Clone()
+        {
+            Console.WriteLine(
+                "Cloning color RGB: {0,3},{1,3},{2,3}",
+                red, green, blue);
+            return this.MemberwiseClone() as ColorPrototype;
+        }
+    }
+}
