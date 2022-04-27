f762064fefbd01134ce15ff419f2c5814c8dd8f3
Quy Nguyen <steve.parker117@gmail.com>
2022-04-27 22:17:56 +0700

    `build`: addition code on implementing the concrete classes and using class for the Brigde design pattern.

--- Bridge_Pattern/Program.cs
+++ Bridge_Pattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using Bridge_Pattern.Structural;
+﻿using Bridge_Pattern.RealWorld;
+using Bridge_Pattern.Structural;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,12 @@ using System.Threading.Tasks;
 
 namespace Bridge_Pattern
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             #region Structural Code
+            Console.WriteLine("****Structural Code****");
             Abstraction ab = new RefinedAbstraction();
 
             //Set implementation and call 
@@ -20,12 +22,23 @@ namespace Bridge_Pattern
             // Change implemention and call 
             ab.Implementor = new ConcreteImplementorB();
             ab.Operation();
-            Console.WriteLine("\n");
+            Console.WriteLine("\n-------------------------\n");
             #endregion
 
             #region Real-world Code
+            Console.WriteLine("***Realworld Code****");
             // Create RefinedAbstraction
-            var customers = new Customers(); 
+            var customers = new Customers();
+            // Set ConcreteImplementor
+            customers.Data = new CustomersData("Chicago");
+            // Exercise the bridge
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Next();
+            customers.Show();
+            customers.Add("Henry Velasquez");
+            customers.ShowAll();
             #endregion
         }
     }
---
+++ Bridge_Pattern/RealWorld/CustomersData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Pattern.RealWorld
+{
+    /// <summary>
+    /// The 'ConcreteImplementor' class
+    /// </summary>
+    public class CustomersData : DataObject
+    {
+        private readonly List<string> customers = new List<string>();
+        private int _current = 0;
+        private string _city;
+
+        public CustomersData(string city)
+        {
+            this._city = city;
+            // Loaded from a database 
+            customers.Add("Jim Jones");
+            customers.Add("Samual Jackson");
+            customers.Add("Allen Good");
+            customers.Add("Ann Stills");
+            customers.Add("Lisa Giolani");
+        }
+        public override void NextRecord()
+        {
+            if(_current <= customers.Count - 1)
+            {
+                _current++;
+            }
+        }
+        public override void PriorRecord()
+        {
+            if(_current > 0)
+            {
+                _current--;
+            }
+        }
+        public override void AddRecord(string _customer)
+        {
+            customers.Add(_customer);
+        }
+        public override void DeleteRecord(string _customer)
+        {
+            customers.Remove(_customer);
+        }
+        public override string GetCurrentRecord()
+        {
+            return customers[_current];
+        }
+        public override void ShowRecord()
+        {
+            Console.WriteLine(customers[_current]);
+        }
+        public override void ShowAllRecords()
+        {
+            Console.WriteLine("Customer City: " + _city);
+
+            foreach(string customerItem in customers)
+            {
+                Console.WriteLine(" " + customerItem);
+            }
+        }
+    }
+}
