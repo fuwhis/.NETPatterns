6988838cd972079426449deb8c6202a01b2c52d6
Quy Nguyen <steve.parker117@gmail.com>
2022-04-27 14:15:01 +0700

    `feat`: add Bridge design pattern on .NETPatterns repo

---
+++ Bridge_Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Structural Code
+
+            #endregion
+
+            #region Real-world Code
+
+            #endregion
+        }
+    }
+}
