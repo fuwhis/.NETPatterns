13aa59e0fdb51e1eaed169da6481bc3057c0f27d
Quy Nguyen <steve.parker117@gmail.com>
2022-05-06 01:03:01 +0700

    `build`: add real-world on math calculator that demonstrated on Proxy design pattern

--- Proxy_Pattern/Program.cs
+++ Proxy_Pattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using Proxy_Pattern.Structural;
+﻿using Proxy_Pattern.RealWorld;
+using Proxy_Pattern.Structural;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,15 +16,21 @@ namespace Proxy_Pattern
         static void Main(string[] args)
         {
             #region Structural Code
-            Console.WriteLine("***Structural Code****\n");
+            Console.WriteLine("***Structural Code Output****\n");
             // Create proxy and request a service
             Proxy proxy = new Proxy();
             proxy.Request();
             #endregion
-            Console.WriteLine("==========================");
+            Console.WriteLine("\n==========================\n");
             #region Real-world Code
-            Console.WriteLine("***Real-world Code****\n");
-
+            Console.WriteLine("***Real-world Code Output****\n");
+            // Create math proxy
+            MathProxy mathProxy = new MathProxy();
+            // Do the math
+            Console.WriteLine("4 + 2 = " + mathProxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + mathProxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + mathProxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + mathProxy.Div(4, 2));
             #endregion
         }
     }
---
+++ Proxy_Pattern/RealWorld/MathProxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pattern.RealWorld
+{
+    public class MathProxy : IMath
+    {
+        private Math math = new Math(); 
+        public double Add(double x, double y)
+        {
+            return math.Add(x, y);
+        }
+        public double Sub(double x, double y)
+        {
+            return math.Sub(x, y);
+        }
+        public double Mul(double x, double y)
+        {
+            return math.Mul(x, y);
+        }
+        public double Div(double x, double y)
+        {
+            return math.Div(x, y);
+        }
+    }
+}
