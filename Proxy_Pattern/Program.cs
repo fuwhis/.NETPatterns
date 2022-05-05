ae517dcd2546d9f6525c6c1bc593e9562a305be6
Quy Nguyen <steve.parker117@gmail.com>
2022-05-05 23:38:47 +0700

    `feat`: add Proxy design pattern of .Net architecture

---
+++ Proxy_Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pattern
+{
+    /// <summary>
+    /// Proxy Design Pattern
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Structural Code
+            Console.WriteLine("***Structural Code****\n");
+            #endregion
+
+            #region Real-world Code
+            Console.WriteLine("***Real-world Code****\n");
+
+            #endregion
+        }
+    }
+}
