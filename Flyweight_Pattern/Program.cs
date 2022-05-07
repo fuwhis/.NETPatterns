f0e517dbd1e9fc3014529acb26010c1ef4e51c10
Quy Nguyen <steve.parker117@gmail.com>
2022-05-07 23:52:18 +0700

    `feat`: add Flyweight design pattern project

---
+++ Flyweight_Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_Pattern
+{
+    /// <summary>
+    /// Flyweight Design Pattern
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Structural Code
+            Console.WriteLine("****Structural Code Output****\n");
+
+            #endregion
+
+            #region Real-world Code
+
+            #endregion
+        }
+    }
+}
