bc7d9a04a6b85f7a7ab733c374e338d75ba51853
Quy Nguyen <steve.parker117@gmail.com>
2022-05-12 23:41:39 +0700

    `build`: add some Flyweights class to implement representative state.

--- Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterA.cs
+++ Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterA.cs
@@ -9,7 +9,21 @@ namespace Flyweight_Pattern.RealWorld
     /// <summary>
     /// A 'ConcreteFlyweight' class
     /// </summary>
-    public class CharacterA: Character
+    public class CharacterA : Character
     {
+        // Constructor
+        public CharacterA()
+        {
+            symbol = 'A';
+            height = 100;
+            width = 120;
+            ascent = 70;
+            descent = 0;
+        }
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(symbol + " (pointsize " + this.pointSize + ")");
+        }
     }
 }
--- Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterZ.cs
+++ Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterZ.cs
@@ -11,6 +11,19 @@ namespace Flyweight_Pattern.RealWorld
     /// </summary>
     public class CharacterZ : Character
     {
-
+        // Constructor
+        public CharacterZ()
+        {
+            symbol = 'Z';
+            height = 100;
+            width = 100;
+            ascent = 68;
+            descent = 0;
+        }
+        public override void Display(int pointSize)
+        {
+            this.pointSize = pointSize;
+            Console.WriteLine(this.symbol + " (pointsize " + this.pointSize + ")");
+        }
     }
 }
