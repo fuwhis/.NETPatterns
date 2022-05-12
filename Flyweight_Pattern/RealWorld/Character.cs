f9eb46bcbde5f03c5fb0ba1afc602363893cc0a1
Quy Nguyen <steve.parker117@gmail.com>
2022-05-12 23:37:24 +0700

    `build`: add an abstract class on Character to define some properties of character on implementation

--- Flyweight_Pattern/RealWorld/Character.cs
+++ Flyweight_Pattern/RealWorld/Character.cs
@@ -11,34 +11,12 @@ namespace Flyweight_Pattern.RealWorld
     /// </summary>
     public abstract class Character
     {
-        private Dictionary<char, Character> characters = new Dictionary<char, Character>();
-
-        public Character GetCharacter(char key)
-        {
-            // Uses "lazy initialization"
-            Character character = null;
-            if(characters.ContainsKey(key))
-            {
-                character = characters[key];
-            }
-            else
-            {
-                switch(key)
-                {
-                    case 'A':
-                        character = new CharacterA();
-                        break;
-                    case 'B':
-                        character = new CharacterB();
-                        break;
-                    //...
-                    case 'Z':
-                        character = new CharacterZ();
-                        break;
-                }
-                characters.Add(key, character);
-            }
-            return character;
-        }
+        protected char symbol;
+        protected int width;
+        protected int height;
+        protected int ascent;
+        protected int descent;
+        protected int pointSize;
+        public abstract void Display(int pointSize);
     }
 }
