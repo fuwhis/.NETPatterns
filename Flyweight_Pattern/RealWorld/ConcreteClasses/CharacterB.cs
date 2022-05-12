db2ab7d5a7f52a97585d4e706ee25617acfb6f2f
Quy Nguyen <steve.parker117@gmail.com>
2022-05-12 23:28:51 +0700

    `build`: add basely classes for real-world implementation on Flyweight pattern

---
+++ Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_Pattern.RealWorld
+{
+    /// <summary>
+    /// A 'ConcreteFlyweight' class
+    /// </summary>
+    public class CharacterB : Character
+    {
+    }
+}
---
+++ Flyweight_Pattern/RealWorld/ConcreteClasses/CharacterZ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight_Pattern.RealWorld
+{
+    /// <summary>
+    /// A 'ConcreteFlyweight' class
+    /// </summary>
+    public class CharacterZ : Character
+    {
+
+    }
+}
